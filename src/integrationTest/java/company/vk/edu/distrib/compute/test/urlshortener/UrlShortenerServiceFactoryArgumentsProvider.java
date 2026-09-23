bb7b6510b418881ac1d678201d0726f8b35d3b07
masha533 <122812831+masha533@users.noreply.github.com>
2026-09-23 22:44:24 +0300

    Use test annotations

--- src/integrationTest/java/company/vk/edu/distrib/compute/test/urlshortener/UrlShortenerServiceFactoryArgumentsProvider.java
+++ src/integrationTest/java/company/vk/edu/distrib/compute/test/urlshortener/UrlShortenerServiceFactoryArgumentsProvider.java
@@ -1,30 +1,16 @@
 package company.vk.edu.distrib.compute.test.urlshortener;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Stream;
-
 import company.vk.edu.distrib.compute.AbstractHttpServiceFactory;
-import company.vk.edu.distrib.compute.urlshortener.DummyUrlShortenerServiceFactory;
-import company.vk.edu.distrib.compute.urlshortener.UrlShortenerService;
-import company.vk.edu.distrib.compute.masha533.urlshortener.UrlShortenerServiceFactory;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.params.provider.Arguments;
+import company.vk.edu.distrib.compute.urlshortener.UrlShortenerTest;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
-import org.junit.jupiter.params.support.ParameterDeclarations;
-import org.junit.platform.commons.util.ReflectionUtils;
 
-public class UrlShortenerServiceFactoryArgumentsProvider implements ArgumentsProvider {
+public class UrlShortenerServiceFactoryArgumentsProvider
+    extends AbstractArgumentsProvider implements ArgumentsProvider {
 
-    private final Collection<Class<? extends AbstractHttpServiceFactory<? extends UrlShortenerService>>> factories =
-        List.of(
-                UrlShortenerServiceFactory.class
+    public UrlShortenerServiceFactoryArgumentsProvider() {
+        super(
+            AbstractArgumentsProvider.findAnnotatedFactories(UrlShortenerTest.class),
+            AbstractHttpServiceFactory.class
         );
-
-    @Override
-    public Stream<? extends Arguments> provideArguments(ParameterDeclarations parameters, ExtensionContext context) {
-        return factories.stream()
-                .map(ReflectionUtils::newInstance)
-                .map(Arguments::of);
     }
 }
--- src/main/java/company/vk/edu/distrib/compute/masha533/urlshortener/UrlShortenerServiceFactory.java
+++ src/main/java/company/vk/edu/distrib/compute/masha533/urlshortener/UrlShortenerServiceFactory.java
@@ -2,9 +2,13 @@ package company.vk.edu.distrib.compute.masha533.urlshortener;
 
 import company.vk.edu.distrib.compute.AbstractHttpServiceFactory;
 import company.vk.edu.distrib.compute.urlshortener.UrlShortenerService;
+import company.vk.edu.distrib.compute.urlshortener.UrlShortenerTest;
+import company.vk.edu.distrib.compute.urlshortener.UrlShortenerAuthTest;
 
 import java.io.IOException;
 
+@UrlShortenerTest
+@UrlShortenerAuthTest
 public class UrlShortenerServiceFactory extends AbstractHttpServiceFactory<UrlShortenerService> {
 
     @Override
